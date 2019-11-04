eb1927f1a5058e40d2e5398c72e90b59841debd4
rookie <xiaodajun@yeezon.com>
2019-11-04 18:25:22 +0800

    first commit

--- test/cegid_customer.test.js
+++ test/cegid_customer.test.js
@@ -6,18 +6,25 @@ describe('test/cegid_customer.test.js', () => {
     let cegidCustomer;
 
     before(() => {
-        cegidCustomer = new CegidCustomer();
+        cegidCustomer = new CegidCustomer({
+            req_url: 'http://40.73.73.57/Y2_TEST/CustomerWcfService.svc?wsdl',
+            user_id: 'ECINTERFACE',
+            password: 'pink123',
+            database_id: 'Y2_THOMASPINK_TEST',
+        });
     });
 
     it('should hello word', async () => {
-        const xml_original = {
-            HelloWorld: {
-                text: 'hello world',
-                clientContext: {
-                    DatabaseId: 'A201',
-                },
-            },
-        };
+        // const xml_original = {
+        //     HelloWorld: {
+        //         text: 'hello world',
+        //         clientContext: {
+        //             DatabaseId: 'A201',
+        //         },
+        //     },
+        // };
+        const xml_original = `<?xml version="1.0" encoding="UTF-8"?>
+        `;
         const result = await cegidCustomer.helloword(xml_original);
         console.log('result', result);
     });
