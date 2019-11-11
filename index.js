05c0923d875c1d3165e1e600e2fa9fe77d3996ef
rookie <xiaodajun@yeezon.com>
2019-11-11 14:51:21 +0800

    feat: index

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    Cegid: require('./lib/cegid'),
+    CegidCustomer: require('./lib/cegid_customer'),
+    CegidItemInventory: require('./lib/cegid_item_inventory')
+};
